990b5f0df6d9d035bcf239234431026d99aaa012
razorlikes <jnieth@live.de>
2016-01-04 23:05:47 +0100

    First test release

---
+++ AppVolumeHotkeys/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AppVolumeHotkeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_Description1 = new System.Windows.Forms.Label();
+            this.button_SetName = new System.Windows.Forms.Button();
+            this.groupBox_Settings = new System.Windows.Forms.GroupBox();
+            this.numericUpDown_VolumeSteps = new System.Windows.Forms.NumericUpDown();
+            this.label_Description3 = new System.Windows.Forms.Label();
+            this.label_ProgramStatus = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_Description2 = new System.Windows.Forms.Label();
+            this.label_Description4 = new System.Windows.Forms.Label();
+            this.label_AppVolume = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label_AppMute = new System.Windows.Forms.Label();
+            this.CueTextBox_AppName = new AppVolumeHotkeys.CueTextBox();
+            this.groupBox_Settings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_VolumeSteps)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_Description1
+            // 
+            this.label_Description1.Location = new System.Drawing.Point(7, 5);
+            this.label_Description1.Name = "label_Description1";
+            this.label_Description1.Size = new System.Drawing.Size(407, 28);
+            this.label_Description1.TabIndex = 0;
+            this.label_Description1.Text = "A simple tool to control the volume of an application in the Windows Audio Mixer." +
+    " Just enter a keyword from the title of the window which volume you want to cont" +
+    "rol.";
+            // 
+            // button_SetName
+            // 
+            this.button_SetName.Location = new System.Drawing.Point(133, 19);
+            this.button_SetName.Name = "button_SetName";
+            this.button_SetName.Size = new System.Drawing.Size(68, 20);
+            this.button_SetName.TabIndex = 2;
+            this.button_SetName.Text = "Set Name";
+            this.button_SetName.UseVisualStyleBackColor = true;
+            this.button_SetName.Click += new System.EventHandler(this.button_SetName_Click);
+            // 
+            // groupBox_Settings
+            // 
+            this.groupBox_Settings.Controls.Add(this.CueTextBox_AppName);
+            this.groupBox_Settings.Controls.Add(this.numericUpDown_VolumeSteps);
+            this.groupBox_Settings.Controls.Add(this.label_Description3);
+            this.groupBox_Settings.Controls.Add(this.label_ProgramStatus);
+            this.groupBox_Settings.Controls.Add(this.button_SetName);
+            this.groupBox_Settings.Location = new System.Drawing.Point(12, 34);
+            this.groupBox_Settings.Name = "groupBox_Settings";
+            this.groupBox_Settings.Size = new System.Drawing.Size(207, 140);
+            this.groupBox_Settings.TabIndex = 2;
+            this.groupBox_Settings.TabStop = false;
+            this.groupBox_Settings.Text = "Settings";
+            // 
+            // numericUpDown_VolumeSteps
+            // 
+            this.numericUpDown_VolumeSteps.Location = new System.Drawing.Point(6, 74);
+            this.numericUpDown_VolumeSteps.Name = "numericUpDown_VolumeSteps";
+            this.numericUpDown_VolumeSteps.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown_VolumeSteps.TabIndex = 6;
+            this.numericUpDown_VolumeSteps.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown_VolumeSteps.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown_VolumeSteps.ValueChanged += new System.EventHandler(this.numericUpDown_VolumeSteps_ValueChanged);
+            // 
+            // label_Description3
+            // 
+            this.label_Description3.AutoSize = true;
+            this.label_Description3.Location = new System.Drawing.Point(49, 77);
+            this.label_Description3.Name = "label_Description3";
+            this.label_Description3.Size = new System.Drawing.Size(145, 13);
+            this.label_Description3.TabIndex = 5;
+            this.label_Description3.Text = "Enter the Volume Steps here.";
+            // 
+            // label_ProgramStatus
+            // 
+            this.label_ProgramStatus.Location = new System.Drawing.Point(3, 42);
+            this.label_ProgramStatus.Name = "label_ProgramStatus";
+            this.label_ProgramStatus.Size = new System.Drawing.Size(203, 27);
+            this.label_ProgramStatus.TabIndex = 3;
+            this.label_ProgramStatus.Text = "No application set. ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 191);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(387, 176);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label_Description2
+            // 
+            this.label_Description2.Location = new System.Drawing.Point(225, 42);
+            this.label_Description2.Name = "label_Description2";
+            this.label_Description2.Size = new System.Drawing.Size(174, 45);
+            this.label_Description2.TabIndex = 4;
+            this.label_Description2.Text = "Preset Hotkeys are \'PageUp\' for Volume UP and \'PageDown\' for Volume DOWN right no" +
+    "w.";
+            // 
+            // label_Description4
+            // 
+            this.label_Description4.AutoSize = true;
+            this.label_Description4.Location = new System.Drawing.Point(224, 98);
+            this.label_Description4.Name = "label_Description4";
+            this.label_Description4.Size = new System.Drawing.Size(48, 26);
+            this.label_Description4.TabIndex = 5;
+            this.label_Description4.Text = "Volume: \r\nMute: ";
+            // 
+            // label_AppVolume
+            // 
+            this.label_AppVolume.AutoSize = true;
+            this.label_AppVolume.Location = new System.Drawing.Point(269, 98);
+            this.label_AppVolume.Name = "label_AppVolume";
+            this.label_AppVolume.Size = new System.Drawing.Size(13, 13);
+            this.label_AppVolume.TabIndex = 6;
+            this.label_AppVolume.Text = "0";
+            // 
+            // label_AppMute
+            // 
+            this.label_AppMute.AutoSize = true;
+            this.label_AppMute.Location = new System.Drawing.Point(257, 111);
+            this.label_AppMute.Name = "label_AppMute";
+            this.label_AppMute.Size = new System.Drawing.Size(21, 13);
+            this.label_AppMute.TabIndex = 7;
+            this.label_AppMute.Text = "No";
+            // 
+            // CueTextBox_AppName
+            // 
+            this.CueTextBox_AppName.Cue = "Search keywords";
+            this.CueTextBox_AppName.Location = new System.Drawing.Point(6, 19);
+            this.CueTextBox_AppName.Name = "CueTextBox_AppName";
+            this.CueTextBox_AppName.Size = new System.Drawing.Size(121, 20);
+            this.CueTextBox_AppName.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(411, 379);
+            this.Controls.Add(this.label_AppMute);
+            this.Controls.Add(this.label_AppVolume);
+            this.Controls.Add(this.label_Description4);
+            this.Controls.Add(this.label_Description2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox_Settings);
+            this.Controls.Add(this.label_Description1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "App Volume Hotkeys";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox_Settings.ResumeLayout(false);
+            this.groupBox_Settings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_VolumeSteps)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Description1;
+        private System.Windows.Forms.Button button_SetName;
+        private System.Windows.Forms.GroupBox groupBox_Settings;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_ProgramStatus;
+        private System.Windows.Forms.Label label_Description2;
+        private System.Windows.Forms.Label label_Description3;
+        private System.Windows.Forms.NumericUpDown numericUpDown_VolumeSteps;
+        private CueTextBox CueTextBox_AppName;
+        private System.Windows.Forms.Label label_Description4;
+        private System.Windows.Forms.Label label_AppVolume;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label_AppMute;
+    }
+}
+
---
+++ AppVolumeHotkeys/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppVolumeHotkeys
+{
+    public partial class Form1 : Form
+    {
+        string AppName;
+        int PID, VolumeSteps;
+        int AppVolume;
+        bool AppMute;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int WM_HOTKEY = 0x0312;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegisterHotKey(this.Handle, 1, 0, (int)Keys.PageUp);
+            RegisterHotKey(this.Handle, 2, 0, (int)Keys.PageDown);
+
+            VolumeSteps = decimal.ToInt32(numericUpDown_VolumeSteps.Value);
+        }
+
+
+        private void button_SetName_Click(object sender, EventArgs e)
+        {
+            AppName = CueTextBox_AppName.Text;
+            foreach (var p in Process.GetProcesses())
+                if (p.MainWindowTitle.ToLower() == AppName.ToLower() | p.MainWindowTitle.ToLower().Contains(AppName.ToLower()))
+                {
+                    textBox1.Text = textBox1.Text + Environment.NewLine + p.MainWindowTitle + " PID = " + p.Id;
+                    label_ProgramStatus.Text = "Application '" + p.MainWindowTitle + "' found and set.";
+                    PID = p.Id;
+                    WriteVolumeLabel();
+                    WriteMuteLabel();
+                    return;
+                }
+                else label_ProgramStatus.Text = "Can't find application '" + CueTextBox_AppName.Text + "'.";
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == 1)
+                VolumeUp();
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == 2)
+                VolumeDown();
+            base.WndProc(ref m);
+        }
+
+        public void VolumeUp()
+        {
+            VolumeMixer.SetApplicationVolume(PID, AppVolume + VolumeSteps);
+            WriteVolumeLabel();
+        }
+
+        public void VolumeDown()
+        {
+            VolumeMixer.SetApplicationVolume(PID, AppVolume - VolumeSteps);
+            WriteVolumeLabel();
+        }
+
+        public void WriteVolumeLabel()
+        {
+            AppVolume = VolumeMixer.GetApplicationVolume(PID);
+            label_AppVolume.Text = AppVolume.ToString();
+        }
+
+        public void WriteMuteLabel()
+        {
+            AppMute = VolumeMixer.GetApplicationMute(PID);
+            if (AppMute == true)
+                label_AppMute.Text = "Yes";
+            else label_AppMute.Text = "No";
+        }
+
+        private void numericUpDown_VolumeSteps_ValueChanged(object sender, EventArgs e)
+        {
+            VolumeSteps = decimal.ToInt32(numericUpDown_VolumeSteps.Value);
+            textBox1.Text = textBox1.Text + Environment.NewLine + VolumeSteps.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 1);
+            UnregisterHotKey(this.Handle, 2);
+        }
+
+        #region c/p
+        public class VolumeMixer
+        {
+            public static int GetApplicationVolume(int pid)
+            {
+                ISimpleAudioVolume volume = GetVolumeObject(pid);
+                if (volume == null)
+                    return 0;
+
+                float level;
+                volume.GetMasterVolume(out level);
+                Marshal.ReleaseComObject(volume);
+                level = level * 100;
+                return Convert.ToInt32(level);
+            }
+
+            public static bool GetApplicationMute(int pid)
+            {
+                ISimpleAudioVolume volume = GetVolumeObject(pid);
+                if (volume == null)
+                    return false;
+
+                bool mute;
+                volume.GetMute(out mute);
+                Marshal.ReleaseComObject(volume);
+                return mute;
+            }
+
+            public static void SetApplicationVolume(int pid, float level)
+            {
+                ISimpleAudioVolume volume = GetVolumeObject(pid);
+                if (volume == null)
+                    return;
+
+                Guid guid = Guid.Empty;
+                volume.SetMasterVolume(level / 100, ref guid);
+                Marshal.ReleaseComObject(volume);
+            }
+
+            public static void SetApplicationMute(int pid, bool mute)
+            {
+                ISimpleAudioVolume volume = GetVolumeObject(pid);
+                if (volume == null)
+                    return;
+
+                Guid guid = Guid.Empty;
+                volume.SetMute(mute, ref guid);
+                Marshal.ReleaseComObject(volume);
+            }
+
+            private static ISimpleAudioVolume GetVolumeObject(int pid)
+            {
+                // get the speakers (1st render + multimedia) device
+                IMMDeviceEnumerator deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+                IMMDevice speakers;
+                deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia, out speakers);
+
+                // activate the session manager. we need the enumerator
+                Guid IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
+                object o;
+                speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
+                IAudioSessionManager2 mgr = (IAudioSessionManager2)o;
+
+                // enumerate sessions for on this device
+                IAudioSessionEnumerator sessionEnumerator;
+                mgr.GetSessionEnumerator(out sessionEnumerator);
+                int count;
+                sessionEnumerator.GetCount(out count);
+
+                // search for an audio session with the required name
+                // NOTE: we could also use the process id instead of the app name (with IAudioSessionControl2)
+                ISimpleAudioVolume volumeControl = null;
+                for (int i = 0; i < count; i++)
+                {
+                    IAudioSessionControl2 ctl;
+                    sessionEnumerator.GetSession(i, out ctl);
+                    int cpid;
+                    ctl.GetProcessId(out cpid);
+
+                    if (cpid == pid)
+                    {
+                        volumeControl = ctl as ISimpleAudioVolume;
+                        break;
+                    }
+                    Marshal.ReleaseComObject(ctl);
+                }
+                Marshal.ReleaseComObject(sessionEnumerator);
+                Marshal.ReleaseComObject(mgr);
+                Marshal.ReleaseComObject(speakers);
+                Marshal.ReleaseComObject(deviceEnumerator);
+                return volumeControl;
+            }
+
+        }
+
+        [ComImport]
+        [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+        internal class MMDeviceEnumerator
+        {
+        }
+
+        internal enum EDataFlow
+        {
+            eRender,
+            eCapture,
+            eAll,
+            EDataFlow_enum_count
+        }
+
+        internal enum ERole
+        {
+            eConsole,
+            eMultimedia,
+            eCommunications,
+            ERole_enum_count
+        }
+
+        [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IMMDeviceEnumerator
+        {
+            int NotImpl1();
+
+            [PreserveSig]
+            int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppDevice);
+
+            // the rest is not implemented
+        }
+
+        [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IMMDevice
+        {
+            [PreserveSig]
+            int Activate(ref Guid iid, int dwClsCtx, IntPtr pActivationParams, [MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
+
+            // the rest is not implemented
+        }
+
+        [Guid("77AA99A0-1BD6-484F-8BC7-2C654C9A9B6F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IAudioSessionManager2
+        {
+            int NotImpl1();
+            int NotImpl2();
+
+            [PreserveSig]
+            int GetSessionEnumerator(out IAudioSessionEnumerator SessionEnum);
+
+            // the rest is not implemented
+        }
+
+        [Guid("E2F5BB11-0570-40CA-ACDD-3AA01277DEE8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IAudioSessionEnumerator
+        {
+            [PreserveSig]
+            int GetCount(out int SessionCount);
+
+            [PreserveSig]
+            int GetSession(int SessionCount, out IAudioSessionControl2 Session);
+        }
+
+        [Guid("87CE5498-68D6-44E5-9215-6DA47EF883D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface ISimpleAudioVolume
+        {
+            [PreserveSig]
+            int SetMasterVolume(float fLevel, ref Guid EventContext);
+
+            [PreserveSig]
+            int GetMasterVolume(out float pfLevel);
+
+            [PreserveSig]
+            int SetMute(bool bMute, ref Guid EventContext);
+
+            [PreserveSig]
+            int GetMute(out bool pbMute);
+        }
+
+
+
+        [Guid("bfb7ff88-7239-4fc9-8fa2-07c950be9c6d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IAudioSessionControl2
+        {
+            // IAudioSessionControl
+            [PreserveSig]
+            int NotImpl0();
+
+            [PreserveSig]
+            int GetDisplayName([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+            [PreserveSig]
+            int SetDisplayName([MarshalAs(UnmanagedType.LPWStr)]string Value, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+            [PreserveSig]
+            int GetIconPath([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+            [PreserveSig]
+            int SetIconPath([MarshalAs(UnmanagedType.LPWStr)] string Value, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+            [PreserveSig]
+            int GetGroupingParam(out Guid pRetVal);
+
+            [PreserveSig]
+            int SetGroupingParam([MarshalAs(UnmanagedType.LPStruct)] Guid Override, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+            [PreserveSig]
+            int NotImpl1();
+
+            [PreserveSig]
+            int NotImpl2();
+
+            // IAudioSessionControl2
+            [PreserveSig]
+            int GetSessionIdentifier([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+            [PreserveSig]
+            int GetSessionInstanceIdentifier([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+            [PreserveSig]
+            int GetProcessId(out int pRetVal);
+
+            [PreserveSig]
+            int IsSystemSoundsSession();
+
+            [PreserveSig]
+            int SetDuckingPreference(bool optOut);
+        }
+        #endregion
+
+    }
+}
