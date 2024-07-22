45c15c919237669a942e2aef9eff70c5c2019258
Jeremy Nieth <jnieth@live.de>
2024-07-22 18:20:58 +0200

    add appcontrol

---
+++ AppVolumeHotkeys/AppControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Win32.Media.Audio;
+
+namespace AppVolumeHotkeys
+{
+    internal class AppControl
+    {
+        private VolumeMixer volumeMixer;
+
+        public ListViewItem listViewItem { get; set; }
+
+        public AppControl(VolumeMixer mixer, ISimpleAudioVolume audioVolume)
+        {
+            volumeMixer = mixer;
+
+            listViewItem = new ListViewItem(new string[] { "TEST1", "TestSub1", "TestSub2" }, 0);
+
+            
+        }
+    }
+}
