607b6d04b1ede90d05c8408a55797608dbce7ba7
Jeremy Nieth <jnieth@live.de>
2024-08-08 20:12:47 +0200

    appcontrol prototyping

--- AppVolumeHotkeys/AppControl.cs
+++ AppVolumeHotkeys/AppControl.cs
@@ -8,19 +8,27 @@ using Windows.Win32.Media.Audio;
 
 namespace AppVolumeHotkeys
 {
-    internal class AppControl
+    public class AppControl
     {
         private VolumeMixer volumeMixer;
 
         public ListViewItem listViewItem { get; set; }
 
-        public AppControl(VolumeMixer mixer, ISimpleAudioVolume audioVolume)
+        public AppControl(VolumeMixer mixer)
         {
             volumeMixer = mixer;
 
             listViewItem = new ListViewItem(new string[] { "TEST1", "TestSub1", "TestSub2" }, 0);
 
             
+        }
+
+        internal void SetInterface(ISimpleAudioVolume volumeInterface)
+        {
+            //volumeInterface.GetMasterVolume(out float volume);
+            //volumeInterface.GetMute(out bool mute);
+            //volumeInterface.SetMasterVolume(0.5f, Guid.Empty);
+            //volumeInterface.SetMute(false, Guid.Empty);
         }
     }
 }
